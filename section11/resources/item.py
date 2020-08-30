7500c3cd9cc0dabb20518a70ba8e3fe82f96f31c
nirvikalpa <experimentalhypothesis@gmail.com>
2020-08-30 23:19:25 +0200

    JWT-optional

--- section11/resources/item.py
+++ section11/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -74,7 +74,16 @@ class Item(Resource):
 class Items(Resource):
     """ Resource for all the items. """
 
+    @jwt_optional # JWT can either be passed or not - user is logged or not
     def get(self):
         """ endpoint for getting all the items """
-        return {"items" : [item.json() for item in ItemModel.find_all()]}, 200
+        user_id = get_jwt_identity() # returns ID or none
+        print(user_id)
+
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {"items": items}, 200   # if he provides, return all
+        return {
+                "items" : [item.name for item in ItemModel.find_all()],
+                "msg": "more data available if you log in"}, 200  # if not, return only names
 
