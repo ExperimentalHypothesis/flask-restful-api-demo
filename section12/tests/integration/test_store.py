666b1fcaa4febe7728e7de83b0f91f6055f1e3f5
nirvikalpa <experimentalhypothesis@gmail.com>
2020-09-02 19:29:24 +0200

    finished integration tests for store [small issue with find all..]

--- section12/tests/integration/test_store.py
+++ section12/tests/integration/test_store.py
@@ -70,8 +70,60 @@ def test_find_by_name(test_client_db):
     store = StoreModel("first")
     store.save_to_db()
 
-    found = store.find_by_name("first")
+    found = StoreModel.find_by_name("first")
     assert found.name == "first"
     assert found.id == 1
     assert list(found.items) == []
+
+    store = StoreModel("second")
+    store.save_to_db()
+    i1 = ItemModel("test", 10, 2)
+    i2 = ItemModel("testing", 11, 2)
+    i1.save_to_db()
+    i2.save_to_db()
+    found = StoreModel.find_by_name("second")
+    assert found.name == "second"
+    assert found.id == 2
+    assert len(list(found.items)) == 2
+    # assert found.items == [
+    #     {
+    #         "id": 1,
+    #         "name": "test",
+    #         "price": 10,
+    #         "store_id": 2,
+    #     },
+    #     {
+    #         "id": 2,
+    #         "name": "testing",
+    #         "price": 11,
+    #         "store_id": 2,
+    #     }
+    # ]
+
+
+def test_find_all(test_client_db):
+    s1 = StoreModel("first")
+    s2 = StoreModel("second")
+    s3 = StoreModel("third")
+    s1.save_to_db()
+    s2.save_to_db()
+    s3.save_to_db()
+    assert len(StoreModel.find_all()) == 3
+
+
+def test_save_delete(test_client_db):
+    s = StoreModel("test")
+    found = StoreModel.find_by_name("test")
+    assert found is None
+
+    s.save_to_db()
+    found = StoreModel.find_by_name("test")
+    assert found is not None
+    assert found.id == 1
+    assert found.name == "test"
+
+    s.delete_from_db()
+    found = StoreModel.find_by_name("test")
+    assert found is None
+
 
